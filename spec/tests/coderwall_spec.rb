c10287f9e5c8d54200a5ea0f7bddce1b933ffa38
con_mame <con_mame@conma.me>
2012-03-30 14:54:28 +0900

    remove unuse files

--- spec/tests/coderwall_spec.rb
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-include CoderWall
-
-describe Coderwall do
-  it { Coderwall.version.should eq '0.0.1' }
-end
